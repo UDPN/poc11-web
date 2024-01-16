974f1527627a878c695fac9063cba654e9e3c657
UDPN-zhangXueFeng <84691916+UDPN-zhangXueFeng@users.noreply.github.com>
2024-01-16 11:12:12 +0800

    ✨ feat: update

--- src/app/core/services/common/metaMask.service.ts
+++ src/app/core/services/common/metaMask.service.ts
@@ -2,18 +2,22 @@
  * @Author: zhangxuefeng
  * @Date: 2024-01-15 14:09:16
  * @LastEditors: zhangxuefeng
- * @LastEditTime: 2024-01-15 15:09:09
- * @Description: 
+ * @LastEditTime: 2024-01-16 10:57:48
+ * @Description:
  */
 import { Injectable } from '@angular/core';
-import { ActivatedRoute, ActivatedRouteSnapshot, Params, Router, UrlSegment } from '@angular/router';
+import {
+  ActivatedRoute,
+  ActivatedRouteSnapshot,
+  Params,
+  Router,
+  UrlSegment
+} from '@angular/router';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 import _ from 'lodash';
 import Web3 from 'web3';
 import { NzModalService } from 'ng-zorro-antd/modal';
-
-
 
 @Injectable({
   providedIn: 'root'
@@ -23,23 +27,38 @@ export class MetaMaskService {
   private metaArray: string[] = [];
   web3: any;
 
-  constructor(private router: Router, private activatedRoute: ActivatedRoute,    private modal: NzModalService,) {
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private modal: NzModalService
+  ) {
     this.web3 = new Web3((window as any).ethereum);
   }
-  checkMeataMask(){
+  async checkMeataMask(): Promise<string[]> {
     if (!this.web3) {
       this.modal.warning({
         nzTitle: 'Warning',
         nzContent: 'Please install and unlock Metamask first!'
       });
-      return;
+      return new Promise(_).then(() => []);
     }
-    this.web3.eth
-      .requestAccounts()
-      .then(() => this.getAccount())
-      .catch(console.error);
+    const permissions = await this.web3.currentProvider.request({
+      method: 'wallet_requestPermissions',
+      params: [{ eth_accounts: {} }]
+    });
+    try {
+      return this.web3.eth
+        .requestAccounts()
+        .then(async () => {
+          this.getAccount();
+          return await this.web3.eth.getAccounts();
+        })
+        .catch(console.error);
+    } catch (e) {
+      return new Promise(_).then(() => []);
+    }
   }
-  async getAccount(){
+  async getAccount() {
     let accounts = await this.web3.eth.getAccounts();
     this.setMetaArray$(accounts);
   }
@@ -50,7 +69,7 @@ export class MetaMaskService {
   setMetaArray$(tabArray: string[]): void {
     this.MetaArray$.next(tabArray);
   }
-  clearMetaArray$():void{
+  clearMetaArray$(): void {
     this.MetaArray$.next([]);
   }
 }
