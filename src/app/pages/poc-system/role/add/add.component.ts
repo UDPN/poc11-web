9aa622b72bd03739d37fbbaa69b7f2d43a9856ae
UDPN-zhangXueFeng <84691916+UDPN-zhangXueFeng@users.noreply.github.com>
2024-07-09 10:08:49 +0800

    update

--- src/app/pages/poc-system/role/add/add.component.ts
+++ src/app/pages/poc-system/role/add/add.component.ts
@@ -126,6 +126,7 @@ export class AddComponent implements OnInit {
 
   treeList() {
     this.roleService.treeList().subscribe((data) => {
+      console.log(data);
       this.dataSource.setData(data);
       // Default Unfold
       this.treeControl.expandAll();
