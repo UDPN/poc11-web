c68b034d5306ba7a5b882e4c28024adf647c8be6
chenyuting <chenyuting@reddatetech.com>
2024-12-20 11:18:27 +0800

    feat: update

---
+++ src/environments/environment.liquidity-prod.ts
@@ -0,0 +1,22 @@
+/*
+ * @Author: zhangxuefeng
+ * @Date: 2024-01-11 14:58:13
+ * @LastEditors: zhangxuefeng
+ * @LastEditTime: 2024-01-31 17:32:07
+ * @Description:
+ */
+export const http = window.location.protocol;
+export const ip = window.location.hostname;
+
+// export const port = "8082";
+// export const localUrl = `http://${ip}:${port}`;
+// // export const localUrl = ``;
+// if (window.location.port === '8082') {
+
+// }
+export const environment = {
+  production: true,
+  localUrl: '',
+  clientName: 'admin',
+  password: 'Abcd1234'
+};
